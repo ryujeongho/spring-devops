0eed16521f72348cad968ce5883a450014cd3671
ryujeongho <ryujh0326@gmail.com>
2024-02-05 17:27:59 +0900

    Feat: 임티추가2

--- src/main/java/com/sh/app/HomeController.java
+++ src/main/java/com/sh/app/HomeController.java
@@ -15,6 +15,6 @@ public class HomeController {
 
     @GetMapping("/")
     public ResponseEntity<?> home() {
-        return ResponseEntity.ok("🙈현재 profile은 %s입니다.".formatted(value));
+        return ResponseEntity.ok("🙈🙈현재 profile은 %s입니다.".formatted(value));
     }
 }
